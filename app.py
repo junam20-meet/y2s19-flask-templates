6835df8f377e896eda47a7a62b2d58e333e862ab
myname21-meet <myname21@meet.mit.edu>
2019-07-30 05:42:25 -0400

    updated

--- app.py
+++ app.py
@@ -3,7 +3,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return "Hello World"
+	food=["mlokhieh","chocolate","pasta","wara2"]
+	oppoeite_day = True
+	least_foods=["fish","shrimp"]
+	return render_template("index.html",foods = food, least = least_foods, oppoeite_day = oppoeite_day)
+
+
 
 if __name__ == '__main__':
    app.run(debug = True)
